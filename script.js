19b40e3d7578648ccd4ed73287f9091257a88819
Deeordie8 <70175400+Deeordie8@users.noreply.github.com>
2024-07-07 19:25:33 +0000

    git push or

--- script.js
+++ script.js
@@ -19,6 +19,47 @@ function handleButtonClick(value) {
     } else if (value === '=') {
         // Perform calculation based on operator
         // Update currentInput and lastInput
+        try {
+            currentInput = eval(currentInput).toString();
+        } catch {
+            currentInput = 'Error';
+        }
+        lastInput = '';
+        operator = '';
+    } else if (value === '.') {
+        if (!currentInput.includes('.')) {
+            currentInput += '.';
+        }
+    } else if (value === 'del') {
+        currentInput = currentInput.substring(0, currentInput.length - 1);
+    } else if (value === 'x') {
+        currentInput += '*';
+    } else if (value === '/') {
+        currentInput += '/';
+    } else if (value === '-') {
+        currentInput += '-';
+    } else if (value === '+') {
+        currentInput += '+';
+    } else if (value === '0') {
+        currentInput += '0';
+    } else if (value === '1') {
+        currentInput += '1';
+    } else if (value === '2') {
+        currentInput += '2';
+    } else if (value === '3') {
+        currentInput += '3';
+    } else if (value === '4') {
+        currentInput += '4';
+    } else if (value === '5') {
+        currentInput += '5';
+    } else if (value === '6') {
+        currentInput += '6';
+    } else if (value === '7') {
+        currentInput += '7';
+    } else if (value === '8') {
+        currentInput += '8';
+    } else if (value === '9') {
+        currentInput += '9';
     } else {
         currentInput = currentInput === '0' ? value : currentInput + value;
     }
@@ -46,3 +87,6 @@ document.getElementById('button_div').addEventListener('click', () => handleButt
 //document.getElementById('button_eq').addEventListener('click', () => handleButtonClick('eval the str fields created (Should end with a number)'));
 //document.getElementById('button_reset').addEventListener('click', () => handleButtonClick('<clear the field>'));
 //document.getElementById('button_del').addEventListener('click', () => handleButtonClick('remove last element of the str shown in the field'));
+document.getElementById('button_eq').addEventListener('click', () => handleButtonClick('='));
+document.getElementById('button_reset').addEventListener('click', () => handleButtonClick('C'));
+document.getElementById('button_del').addEventListener('click', () => handleButtonClick('del'));
