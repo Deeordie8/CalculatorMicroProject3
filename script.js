4302b4e58bbfa3af545ef8d03f22d3f8b6d38f63
Deeordie8 <70175400+Deeordie8@users.noreply.github.com>
2024-07-07 18:25:40 +0000

    comments

--- script.js
+++ script.js
@@ -27,3 +27,22 @@ function handleButtonClick(value) {
 
 // Add event listeners to your calculator buttons
 // Example: document.getElementById('button1').addEventListener('click', () => handleButtonClick('1'));
+document.getElementById('button1').addEventListener('click', () => handleButtonClick('1'));
+document.getElementById('button2').addEventListener('click', () => handleButtonClick('2'));
+document.getElementById('button3').addEventListener('click', () => handleButtonClick('3'));
+document.getElementById('button4').addEventListener('click', () => handleButtonClick('4'));
+document.getElementById('button5').addEventListener('click', () => handleButtonClick('5'));
+document.getElementById('button6').addEventListener('click', () => handleButtonClick('6'));
+document.getElementById('button7').addEventListener('click', () => handleButtonClick('7'));
+document.getElementById('button8').addEventListener('click', () => handleButtonClick('8'));
+document.getElementById('button9').addEventListener('click', () => handleButtonClick('9'));
+document.getElementById('button0').addEventListener('click', () => handleButtonClick('0'));
+document.getElementById('button_add').addEventListener('click', () => handleButtonClick('+'));
+document.getElementById('button_minus').addEventListener('click', () => handleButtonClick('-'));
+document.getElementById('button_mult').addEventListener('click', () => handleButtonClick('*'));
+document.getElementById('button_point').addEventListener('click', () => handleButtonClick('.'));
+document.getElementById('button_div').addEventListener('click', () => handleButtonClick('/'));
+// Below are special case handle cases and need to look into these separately
+//document.getElementById('button_eq').addEventListener('click', () => handleButtonClick('eval the str fields created (Should end with a number)'));
+//document.getElementById('button_reset').addEventListener('click', () => handleButtonClick('<clear the field>'));
+//document.getElementById('button_del').addEventListener('click', () => handleButtonClick('remove last element of the str shown in the field'));
